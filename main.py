44facedd8ec8742ff989287b33b62cadf34646b5
bwallker <benbwall@gmail.com>
2021-02-23 23:40:46 +0200

    - Did loads of stuff - Bot almost works as intended

--- main.py
+++ main.py
@@ -1,20 +1,118 @@
 # bot.py
 import json
 import discord
+from discord.ext import commands
 with open ("config.json", "r") as json_config_file:
     configData = json.load(json_config_file)
     print("Config file read")
     json_config_file.close()
 TOKEN = configData['BOT_TOKEN']
-print("Printing config token")
-print(TOKEN)
-print("Printing whole config file")
-print(configData)
+GUILD_ID = int(configData['GUILD_ID'])
+PREFIX = configData['PREFIX']
+PLEB_ID = configData['ROLE1_ID']
+BOT_ID = configData['ROLE2_ID']
+BOT_CHANNEL_ID = int(configData['BOT_CHANNEL_ID'])
+PLEB_CHANNEL_ID = int(configData['PLEB_CHANNEL_ID'])
+ROLELESS_CHANNEL_ID = int(configData['ROLELESS_CHANNEL_ID'])
+bot = commands.Bot(command_prefix=PREFIX)
+BOT_CHANNEL = None
+PLEB_CHANNEL = None
+ROLELESS_CHANNEL = None
+GUILD = None
 
-client = discord.Client()
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    global GUILD, BOT_CHANNEL, BOT_CHANNEL_ID, PLEB_CHANNEL, PLEB_CHANNEL_ID, ROLELESS_CHANNEL, ROLELESS_CHANNEL_ID
+    for guild in bot.guilds:
+        if guild.id == GUILD_ID:
+            GUILD = guild
+            break
+    for channel in guild.voice_channels:
+        if channel.id == BOT_CHANNEL_ID:
+            BOT_CHANNEL = channel
+            print(BOT_CHANNEL)
+        elif channel.id == PLEB_CHANNEL_ID:
+            PLEB_CHANNEL = channel
+            print(PLEB_CHANNEL)
+        elif channel.id == ROLELESS_CHANNEL_ID:
+            ROLELESS_CHANNEL = channel
+            print(ROLELESS_CHANNEL)
 
-client.run(TOKEN)
+
+    print(f'{bot.user} has connected to Discord!')
+    print(f'Server name is {guild.name} and id is {guild.id}')
+    print('Standing by')
+def calculateChannels(member, mode):
+    global GUILD, BOT_CHANNEL, PLEB_CHANNEL, ROLELESS_CHANNEL
+    if (mode == "member left"):
+        print(f'{member} left the server!')
+
+    elif (mode == "member joined"):
+        print(f'{member} joined the server!')
+
+    elif (mode == "forced update"):
+        print('Forced update commencing')
+
+    print('updating channels...')
+
+    plebAmount = 0
+    botAmount = 0
+    allMembers = GUILD.member_count
+    roleLessAmount = 0
+    print(f'All members: {allMembers}')
+    for member in GUILD.members:
+        roleLessCheck = 0
+        for role in member.roles:
+            if role.id == PLEB_ID:
+                roleLessCheck = 1
+                plebAmount+=1
+            if role.id == BOT_ID:
+                roleLessCheck = 1
+                botAmount+=1
+            if roleLessCheck == 0:
+                roleLessAmount+=1
+    print(f'Plebs: {plebAmount}')
+    print(f'Bots: {botAmount}')
+    print(f'Roleless: {roleLessAmount}')
+    botTemp = "Bottar: " + str(botAmount)
+    plebTemp = "Pöblar: " + str(plebAmount)
+    roleLessTemp = "Utan roller: " + str(roleLessAmount)
+    returnArr = [botTemp, plebTemp, roleLessTemp]
+    return returnArr
+
+
+
+async def on_member_join(member):
+    returnArr = calculateChannels(member, "member joined")
+    botTemp = returnArr[0]
+    plebtemp = returnArr[1]
+    roleLessTemp = returnArr[2]
+    await BOT_CHANNEL.edit(name=botTemp)
+    await PLEB_CHANNEL.edit(name=plebTemp)
+    await ROLELESS_CHANNEL.edit(name=roleLessTemp)
+    print('Channels updated!')
+
+
+async def on_member_remove(member):
+    returnArr = calculateChannels(member, "member left")
+    botTemp = returnArr[0]
+    plebtemp = returnArr[1]
+    roleLessTemp = returnArr[2]
+    await BOT_CHANNEL.edit(name=botTemp)
+    await PLEB_CHANNEL.edit(name=plebTemp)
+    await ROLELESS_CHANNEL.edit(name=roleLessTemp)
+    print('Channels updated!')
+
+@bot.command(name='forceUpdate',help='Forces an update to channel names')
+async def forceUpdate(ctx):
+    returnArr = calculateChannels(None, "forced update")
+    botTemp = returnArr[0]
+    plebTemp = returnArr[1]
+    roleLessTemp = returnArr[2]
+    await BOT_CHANNEL.edit(name=botTemp)
+    await PLEB_CHANNEL.edit(name=plebTemp)
+    await ROLELESS_CHANNEL.edit(name=roleLessTemp)
+    await ctx.send("Channels updated!")
+    print('Channels updated!')
+bot.run(TOKEN)
