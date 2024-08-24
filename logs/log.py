e3c06c348bcd38034def3f4a80c43c27935dbea6
Ben Bergenwall <58291118+Bwallker@users.noreply.github.com>
2024-08-24 23:02:44 +0300

    Update log.py

--- logs/log.py
+++ logs/log.py
@@ -4,12 +4,12 @@ cwd = os.getcwd()
 PATH_TO_LOG_FILES = f'{cwd}/count_keeper_data/logs'
     
 def print(message: str):
-    log(message)
+    # log(message)
     std.print(message)
 
 
 def log(message: str):
-    global PATH_TO_LOG_FILES
-    with open(f'{PATH_TO_LOG_FILES}/latest.log', 'a') as latest_log:
-        latest_log.write(f'{message}\n')
+    # global PATH_TO_LOG_FILES
+    # with open(f'{PATH_TO_LOG_FILES}/latest.log', 'a') as latest_log:
+        # latest_log.write(f'{message}\n')
         
