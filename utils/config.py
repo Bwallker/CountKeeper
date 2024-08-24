e345fdfbb3e19523e0f38d6a5b10125dd7da7b04
Ben Bergenwall <58291118+Bwallker@users.noreply.github.com>
2024-08-24 23:02:04 +0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,9 +1,20 @@
 import json
 from logs.log import print
+from os import getenv
+def load_var(var_name: str, mapper = None) -> None:
+    v = getenv(var_name)
+    if mapper is not None:
+        v = mapper(v)
+    globals()[var_name] = v
+
 def load():
     with open("count_keeper_data/config.json") as json_config_file:
         config_data = json.load(json_config_file)
     for key, value in config_data.items():
         globals()[key] = value
     print("Config file read")
-load()
+load_var('LOG_LIMIT', int)
+load_var('BOT_TOKEN')
+load_var('DEFAULT_PREFIX')
+load_var('BOT_WEBSITE')
+load_var('BOT_NAME')
